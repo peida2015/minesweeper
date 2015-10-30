652e6105872d52e07a2e18d9c10c2884cf0cf542
App Academy Student <student@appacademy.io>
2015-10-30 12:00:02 -0700

    Setup Tile class

---
+++ minesweeper.rb
@@ -0,0 +1,33 @@
+class Tile
+
+  attr_reader :value, :pos
+  attr_accessor :revealed
+
+  NEIGHBOR_STEPS = [[0,  1],
+                    [0, -1],
+                    [-1, 1],
+                    [-1, 0],
+                    [-1,-1],
+                    [1,  1],
+                    [1,  0],
+                    [1, -1] ]
+
+  def initialize(value = nil, pos = [0,0])
+    @value = value
+    @pos = pos
+    @revealed = false
+  end
+
+  def reveal
+    @revealed = true
+    @value
+  end
+
+  def neighbors
+    NEIGHBOR_STEPS.map { |step| p [pos[0] + step[0], pos[1] + step[1]]  }
+  end
+
+  def neighbor_bomb_count
+    neighbors.count {|neighbor| neighbor.value == :B }
+  end
+end
